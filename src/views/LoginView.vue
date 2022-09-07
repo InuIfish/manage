10391687c6be17eb76127b2e142c9a6bce880d53
InuIfish <979712216@qq.com>
2022-09-07 22:06:22 +0800

    new file:   auto-imports.d.ts     new file:   components.d.ts     modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	new file:   src/assets/IMG_0073.JPG 	modified:   src/main.ts 	modified:   src/router/index.ts 	new file:   src/views/LoginView.vue 	modified:   vue.config.js

---
+++ src/views/LoginView.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="login-box">
+        <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="80px"
+    class="demo-ruleForm"
+  >
+  <h2>后台系统</h2>
+    <el-form-item label="账号" prop="account">
+      <el-input v-model="ruleForm.account"  autocomplete="off" />
+    </el-form-item>
+    <el-form-item label='密码' prop="password">
+      <el-input
+        v-model="ruleForm.password"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
+    
+    <el-form-item>
+      <el-button class = 'loginBtn' type="primary" @click="submitForm(ruleFormRef)"
+        >登陆</el-button
+      >
+      <el-button class = 'loginBtn' @click="resetForm(ruleFormRef)">重置</el-button>
+    </el-form-item>
+  </el-form>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+
+export default defineComponent({
+    setup () {
+        const data = reactive({
+            ruleForm:{
+                account:"",
+                password: '',
+            },
+            rules:{account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            { min: 3, max: 5, message: '账号长度在3-5个字', trigger: 'blur' }],
+            password:[
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 8, max: 16, message: '密码长度为8-16个字', trigger: 'blur' }]
+            }
+        })
+
+        return {
+            ...toRefs(data),
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.login-box{
+    width: 100%;
+    height: 100%;
+    background: url('../assets/IMG_0073.JPG');
+    padding: 1px;
+    text-align: center;
+    .demo-ruleForm{
+    margin: 200px auto;
+    width: 500px;
+    background: skyblue;
+    padding: 40px;
+    border-radius: 40px;
+    .loginBtn{
+        width: 48%;
+    }
+    h2{
+        margin-bottom: 10px;
+    }
+}
+}
+
+</style>
